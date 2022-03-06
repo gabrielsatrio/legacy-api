37cdeafd32aaa1278514f08884bcbd13fcf5d102
Gabriel Satrio Wibowo <gabriel@ateja.co.id>
2022-03-06 22:33:33 +0700

    initial commit

---
+++ src/modules/manufacturing/entities/pengiriman-kain.ts
@@ -0,0 +1,54 @@
+import { Field, ObjectType } from 'type-graphql';
+import { BaseEntity, Column, Entity, PrimaryColumn } from 'typeorm';
+
+@Entity('GBR_PENGIRIMAN_KAIN')
+@ObjectType()
+export class PengirimanKain extends BaseEntity {
+  @Field()
+  @PrimaryColumn({ name: 'ID' })
+  id!: number;
+
+  @Field()
+  @Column({ name: 'CONTRACT' })
+  contract!: string;
+
+  @Field()
+  @Column({ name: 'TGL' })
+  tgl!: Date;
+
+  @Field()
+  @Column({ name: 'SHIFT' })
+  shift!: string;
+
+  @Field()
+  @Column({ name: 'LOT_BATCH_NO' })
+  lotBatchNo!: string;
+
+  @Field()
+  @Column({ name: 'CATEGORY' })
+  category!: string;
+
+  @Field()
+  @Column({ name: 'DESIGN' })
+  design!: string;
+
+  @Field()
+  @Column({ name: 'JOB_ORDER' })
+  jobOrder!: string;
+
+  @Field()
+  @Column({ name: 'QTY_KP' })
+  qtyKp!: number;
+
+  @Field()
+  @Column({ name: 'QTY_REAL' })
+  qtyReal!: number;
+
+  @Field()
+  @Column({ name: 'CREATED_AT' })
+  createdAt!: Date;
+
+  @Field()
+  @Column({ name: 'CREATED_BY' })
+  createdBy!: string;
+}
