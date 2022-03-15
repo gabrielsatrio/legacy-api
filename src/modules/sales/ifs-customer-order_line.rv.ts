d32fe3836f3f18e1a9633d21204f916aa2feb8ee
Giovani Chandra <giovanichandra@ateja.co.id>
2022-03-15 10:23:46 +0700

    ok

--- src/modules/sales/ifs-customer-order_line.rv.ts
+++ src/modules/sales/ifs-customer-order_line.rv.ts
@@ -1,5 +1,6 @@
 import { isAuth } from '@/middlewares/is-auth';
 import { Arg, Query, Resolver, UseMiddleware } from 'type-graphql';
+import { In, Like } from 'typeorm';
 import { IfsCustomerOrderLineView } from './entities/ifs-customer-order_line.vw';
 
 @Resolver(IfsCustomerOrderLineView)
@@ -7,10 +8,10 @@ export class IfsCustomerOrderLineViewResolver {
   @Query(() => [IfsCustomerOrderLineView], { nullable: true })
   @UseMiddleware(isAuth)
   async getCustomerOrderLineByOrderNo(
-    @Arg('orderNo') orderNo: string
-  ): Promise<IfsCustomerOrderLineView[]> {
+    @Arg('orderNo', () => [String]) orderNo: string[]
+  ): Promise<IfsCustomerOrderLineView[] | undefined> {
     return await IfsCustomerOrderLineView.find({
-      where: { orderNo },
+      where: { orderNo: In(orderNo), partNo: Like('F%') },
       order: { lineNo: 'ASC' }
     });
   }
