22431eacfde48c99f07ed7b6017b1f09fef354fc
christian <christianwibisono@ateja.co.id>
2022-03-21 04:44:22 +0000

    fin crud ket lost time

---
+++ src/modules/manufacturing/ket-lost-time.rv.ts
@@ -0,0 +1,80 @@
+import { isAuth } from '@/middlewares/is-auth';
+import { mapError } from '@/utils/map-error';
+import { Arg, Mutation, Query, Resolver, UseMiddleware } from 'type-graphql';
+import { In } from 'typeorm';
+import { KeteranganLostTime } from './entities/ket-lost-time';
+import { KeteranganLostTimeInput } from './ket-lost-time.in';
+
+@Resolver(KeteranganLostTime)
+export class KeteranganLostTimeResolver {
+  @Query(() => [KeteranganLostTime], { nullable: true })
+  @UseMiddleware(isAuth)
+  async getKeteranganLostTime(
+    @Arg('contract', () => [String]) contract: string[]
+  ): Promise<KeteranganLostTime[] | undefined> {
+    return await KeteranganLostTime.find({
+      contract: In(contract)
+    });
+  }
+
+  @Mutation(() => KeteranganLostTime)
+  @UseMiddleware(isAuth)
+  async createKeteranganLostTime(
+    @Arg('input') input: KeteranganLostTimeInput
+  ): Promise<KeteranganLostTime | undefined> {
+    try {
+      const check = await KeteranganLostTime.findOne({
+        contract: input.contract,
+        lostTime: input.lostTime
+      });
+
+      if (check) throw new Error('Data Already Exists');
+
+      const data = KeteranganLostTime.create({
+        ...input
+      });
+      const results = await KeteranganLostTime.save(data);
+      return results;
+    } catch (err) {
+      throw new Error(mapError(err));
+    }
+  }
+
+  @Mutation(() => KeteranganLostTime, { nullable: true })
+  @UseMiddleware(isAuth)
+  async updateKeteranganLostTime(
+    @Arg('input') input: KeteranganLostTimeInput
+  ): Promise<KeteranganLostTime | undefined | number> {
+    try {
+      const data = await KeteranganLostTime.findOne({
+        contract: input.contract,
+        lostTime: input.lostTime
+      });
+      if (!data) throw new Error('No data found.');
+      KeteranganLostTime.merge(data, input);
+      const results = await KeteranganLostTime.save(data);
+      return results;
+    } catch (err) {
+      throw new Error(mapError(err));
+    }
+  }
+
+  @Mutation(() => KeteranganLostTime)
+  @UseMiddleware(isAuth)
+  async deleteKeteranganLostTime(
+    @Arg('contract') contract: string,
+    @Arg('lostTime') lostTime: string
+  ): Promise<KeteranganLostTime> {
+    try {
+      const data = await KeteranganLostTime.findOne({
+        contract,
+        lostTime
+      });
+      if (!data) throw new Error('No data found.');
+      await KeteranganLostTime.delete({ contract, lostTime });
+      return data;
+    } catch (err) {
+      throw new Error(mapError(err));
+    }
+  }
+}
