16bff5d99928950f4cee01f65c5f0d07a1857512
Herry Roby <herry.roby@gmail.com>
2022-03-31 17:15:59 +0700

    Add new field resolver for **labelKey**

--- src/modules/manufacturing/entities/department.vw.ts
+++ src/modules/manufacturing/entities/department.vw.ts
@@ -11,4 +11,9 @@ export class DepartmentView extends BaseEntity {
   @Field()
   @Column({ name: 'DESCRIPTION' })
   description!: string;
+
+  @Field()
+  altDescription(): string {
+    return `${this.description} (${this.departmentId})`;
+  }
 }
