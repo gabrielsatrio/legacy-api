ae0ee01ea2dd972a19021ddfd5901ac217f5e00c
Christian Wibisono <christianwibisono@ateja.co.id>
2022-06-27 13:52:54 +0700

    fix: transport task agt

--- src/modules/manufacturing/tt-head.rv.ts
+++ src/modules/manufacturing/tt-head.rv.ts
@@ -28,30 +28,64 @@ export class TTHeadResolver {
   async getPartTTBahan(
     @Arg('contract') contract: string
   ): Promise<IfsInventoryPartView[] | undefined> {
-    return await IfsInventoryPartView.createQueryBuilder('IP')
-      .where('IP.CONTRACT = :contract', { contract: contract })
-      .andWhere(
-        new Brackets((qb) => {
-          qb.where("IP.PART_NO like 'Y%'")
-            .orWhere("IP.PART_NO like 'T%'")
-            .orWhere("IP.PART_NO like 'MT%'")
-            .orWhere("IP.PART_NO like 'MY%'")
-            .orWhere("IP.PART_NO like 'C%'")
-            .orWhere("IP.PART_NO like 'BPPL%'")
-            .orWhere("IP.PART_NO like 'NW%'")
-            .orWhere("IP.PART_NO like 'BA%'")
-            .orWhere("IP.PART_NO like 'MWV%'")
-            .orWhere("IP.PART_NO like 'MNV%'")
-            .orWhere("IP.PART_NO like 'FA%'")
-            .orWhere("IP.PART_NO like 'FB%'")
-            .orWhere("IP.PART_NO like 'FX%'")
-            .orWhere("IP.PART_NO like 'FZ%'")
-            .orWhere("IP.PART_NO like 'MF%'")
-            .orWhere("IP.PART_NO like 'BPRK%'");
-        })
-      )
-      .andWhere(`ip.PART_STATUS in('A','I')`)
-      .getMany();
+    let sql = '';
+    if (contract !== 'AGT') {
+      return await IfsInventoryPartView.createQueryBuilder('IP')
+        .where('IP.CONTRACT = :contract', { contract: contract })
+        .andWhere(
+          new Brackets((qb) => {
+            qb.where("IP.PART_NO like 'Y%'")
+              .orWhere("IP.PART_NO like 'T%'")
+              .orWhere("IP.PART_NO like 'MT%'")
+              .orWhere("IP.PART_NO like 'MY%'")
+              .orWhere("IP.PART_NO like 'C%'")
+              .orWhere("IP.PART_NO like 'BPPL%'")
+              .orWhere("IP.PART_NO like 'NW%'")
+              .orWhere("IP.PART_NO like 'BA%'")
+              .orWhere("IP.PART_NO like 'MWV%'")
+              .orWhere("IP.PART_NO like 'MNV%'")
+              .orWhere("IP.PART_NO like 'FA%'")
+              .orWhere("IP.PART_NO like 'FB%'")
+              .orWhere("IP.PART_NO like 'FX%'")
+              .orWhere("IP.PART_NO like 'FZ%'")
+              .orWhere("IP.PART_NO like 'MF%'")
+              .orWhere("IP.PART_NO like 'BPRK%'");
+          })
+        )
+        .andWhere(`ip.PART_STATUS in('A','I')`)
+        .getMany();
+    } else {
+      sql = `
+      SELECT part_no      AS "partNo",
+             contract     AS "contract",
+             description  AS "description",
+             unit_meas    AS "unitMeas",
+             part_status  AS "partStatus",
+             objid        AS "objId"
+      FROM   inventory_part@ifs8agt
+      WHERE  contract = :contract
+      and    (PART_NO like 'Y%' OR
+              PART_NO like 'T%' OR
+              PART_NO like 'MT%' OR
+              PART_NO like 'MY%' OR
+              PART_NO like 'C%' OR
+              PART_NO like 'BPPL%' OR
+              PART_NO like 'NW%' OR
+              PART_NO like 'BA%' OR
+              PART_NO like 'MWV%' OR
+              PART_NO like 'MNV%' OR
+              PART_NO like 'FA%' OR
+              PART_NO like 'FB%' OR
+              PART_NO like 'FX%' OR
+              PART_NO like 'FZ%' OR
+              PART_NO like 'MF%' OR
+              PART_NO like 'BPRK%' )
+      AND    part_status in('A','I')
+
+    `;
+      const result = await ifs.query(sql, [contract]);
+      return result;
+    }
   }
 
   @Query(() => [IfsInventoryPartView], { nullable: true })
@@ -60,19 +94,39 @@ export class TTHeadResolver {
     @Arg('contract') contract: string,
     @Arg('partNo') partNo: string
   ): Promise<IfsInventoryPartView[] | undefined> {
-    return await IfsInventoryPartView.createQueryBuilder('IP')
-      .where('IP.CONTRACT = :contract', { contract: contract })
-      .andWhere(
-        new Brackets((qb) => {
-          qb.where("IP.PART_NO like '%'||UPPER(:partNo)||'%'", {
-            partNo: partNo
-          }).orWhere("IP.DESCRIPTION like '%'||UPPER(:partNo)||'%'", {
-            partNo: partNo
-          });
-        })
-      )
-      .andWhere(`ip.PART_STATUS in('A','I')`)
-      .getMany();
+    let sql = '';
+    if (contract !== 'AGT') {
+      return await IfsInventoryPartView.createQueryBuilder('IP')
+        .where('IP.CONTRACT = :contract', { contract: contract })
+        .andWhere(
+          new Brackets((qb) => {
+            qb.where("IP.PART_NO like '%'||UPPER(:partNo)||'%'", {
+              partNo: partNo
+            }).orWhere("IP.DESCRIPTION like '%'||UPPER(:partNo)||'%'", {
+              partNo: partNo
+            });
+          })
+        )
+        .andWhere(`ip.PART_STATUS in('A','I')`)
+        .getMany();
+    } else {
+      sql = `
+      SELECT part_no      AS "partNo",
+             contract     AS "contract",
+             description  AS "description",
+             unit_meas    AS "unitMeas",
+             part_status  AS "partStatus",
+             objid        AS "objId"
+      FROM   inventory_part@ifs8agt
+      WHERE  contract = :contract
+      and    (PART_NO like '%'||UPPER(:partNo)||'%' OR
+              description like '%'||UPPER(:partNo)||'%' )
+      AND    part_status in('A','I')
+
+    `;
+      const result = await ifs.query(sql, [contract, partNo]);
+      return result;
+    }
   }
 
   @Query(() => String, { nullable: true })
@@ -81,37 +135,59 @@ export class TTHeadResolver {
     @Arg('contract') contract: string,
     @Arg('partNo') partNo: string
   ): Promise<string | undefined> {
-    const data = await IfsInventoryPartInStockView.createQueryBuilder('IPIS')
-      .where('IPIS.CONTRACT = :contract', { contract: contract })
-      .select('SUM(IPIS.QTY_ONHAND - IPIS.QTY_RESERVED)', 'qtyAvail')
-      .andWhere('IPIS.PART_NO = :partNo', { partNo: partNo })
-      .andWhere(
-        new Brackets((qb) => {
-          qb.where(`IPIS.LOCATION_NO like 'RM%'`).orWhere(
-            `IPIS.LOCATION_NO like 'FG%'`
-          );
-        })
-      )
-      .andWhere(
-        `IPIS.LOCATION_NO not like case when IPIS.CONTRACT in('AMI') then 'RM%JUAL' else 'NULL' end`
-      )
-      .andWhere(
-        `IPIS.LOCATION_NO not like case when IPIS.CONTRACT in('AT2','AMI') then 'RM%NG' else 'NULL' end`
-      )
-      .andWhere(
-        `IPIS.LOCATION_NO not like case when IPIS.CONTRACT in('AT2','AMI','AT4','AT1','ATD','ATS') then 'RM%QA1' else 'NULL' end`
-      )
-      .andWhere(
-        `IPIS.LOCATION_NO not like case when IPIS.CONTRACT in('AT2','AT1') then 'RM%RTR' else 'NULL' end`
-      )
-      .andWhere(
-        `IPIS.LOCATION_NO not like case when IPIS.CONTRACT in('AT2','AMI','AT4','AT1','ATD','ATS') then 'RM%QA2' else 'NULL' end`
-      )
-      .andWhere('IPIS.QTY_ONHAND > 0')
-      .andWhere('IPIS.QTY_ONHAND != IPIS.QTY_RESERVED')
-      .getRawOne();
+    let sql;
+    if (contract !== 'AGT') {
+      const data = await IfsInventoryPartInStockView.createQueryBuilder('IPIS')
+        .where('IPIS.CONTRACT = :contract', { contract: contract })
+        .select('SUM(IPIS.QTY_ONHAND - IPIS.QTY_RESERVED)', 'qtyAvail')
+        .andWhere('IPIS.PART_NO = :partNo', { partNo: partNo })
+        .andWhere(
+          new Brackets((qb) => {
+            qb.where(`IPIS.LOCATION_NO like 'RM%'`).orWhere(
+              `IPIS.LOCATION_NO like 'FG%'`
+            );
+          })
+        )
+        .andWhere(
+          `IPIS.LOCATION_NO not like case when IPIS.CONTRACT in('AMI') then 'RM%JUAL' else 'NULL' end`
+        )
+        .andWhere(
+          `IPIS.LOCATION_NO not like case when IPIS.CONTRACT in('AT2','AMI') then 'RM%NG' else 'NULL' end`
+        )
+        .andWhere(
+          `IPIS.LOCATION_NO not like case when IPIS.CONTRACT in('AT2','AMI','AT4','AT1','ATD','ATS') then 'RM%QA1' else 'NULL' end`
+        )
+        .andWhere(
+          `IPIS.LOCATION_NO not like case when IPIS.CONTRACT in('AT2','AT1') then 'RM%RTR' else 'NULL' end`
+        )
+        .andWhere(
+          `IPIS.LOCATION_NO not like case when IPIS.CONTRACT in('AT2','AMI','AT4','AT1','ATD','ATS') then 'RM%QA2' else 'NULL' end`
+        )
+        .andWhere('IPIS.QTY_ONHAND > 0')
+        .andWhere('IPIS.QTY_ONHAND != IPIS.QTY_RESERVED')
+        .getRawOne();
 
-    return data.qtyAvail;
+      return data.qtyAvail;
+    } else {
+      sql = `
+        SELECT SUM(QTY_ONHAND - QTY_RESERVED) AS "qtyAvail"
+        FROM   INVENTORY_PART_IN_STOCK@ifs8agt
+        WHERE  contract = :contract
+        AND    part_no =: partNo
+        AND    (location_no like 'RM%'
+                or location_no like 'FG%')
+        and location_no not like case when contract in('AMI') then 'RM%JUAL' else 'NULL' end
+        and location_no not like case when contract in('AT2','AMI') then 'RM%NG' else 'NULL' end
+        and location_no not like case when contract in('AT2','AMI','AT4','AT1','ATD','ATS','AGT') then 'RM%QA1' else 'NULL' end
+        and location_no not like case when contract in('AT2','AT1','AGT') then 'RM%RTR' else 'NULL' end
+        and location_no not like case when contract in('AT2','AMI','AT4','AT1','ATD','ATS') then 'RM%QA2' else 'NULL' end
+        and qty_onhand > 0
+        and qty_reserved < qty_onhand
+        order by receipt_date
+      `;
+      const result = await ifs.query(sql, [contract, partNo]);
+      return result[0].qtyAvail;
+    }
   }
 
   @Mutation(() => TransportTaskHead)
