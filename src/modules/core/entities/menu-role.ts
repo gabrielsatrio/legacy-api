e7e71b2363c141f36dee359b12d6b8efb3f6e385
Christian Wibisono <christianwibisono@ateja.co.id>
2022-06-28 17:30:18 +0700

    feat: menu role backend

---
+++ src/modules/core/entities/menu-role.ts
@@ -0,0 +1,18 @@
+import { Field, ObjectType } from 'type-graphql';
+import { BaseEntity, Column, Entity, PrimaryColumn } from 'typeorm';
+
+@Entity('ATJ_APP_ROLE')
+@ObjectType()
+export class MenuRole extends BaseEntity {
+  @Field()
+  @PrimaryColumn({ name: 'MENU_ID' })
+  menuId!: number;
+
+  @Field()
+  @PrimaryColumn({ name: 'DEPT' })
+  dept!: string;
+
+  @Field()
+  @Column({ name: 'MENU_NAME' })
+  menuName!: string;
+}
