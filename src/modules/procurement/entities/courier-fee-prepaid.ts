39ed9afb0460c5f48c2c3efb459d79b8aa5bd6b8
angellia <angellia@ateja.co.id>
2022-07-15 11:47:02 +0700

    feat: courier fee prepaid module

---
+++ src/modules/procurement/entities/courier-fee-prepaid.ts
@@ -0,0 +1,90 @@
+import { Field, Int, ObjectType } from 'type-graphql';
+import { BaseEntity, Column, Entity, PrimaryColumn } from 'typeorm';
+
+@Entity('ANG_COURIER_FEE_PREPAID')
+@ObjectType()
+export class CourierFeePrepaid extends BaseEntity {
+  @Field(() => Int)
+  @PrimaryColumn({ name: 'ID' })
+  id!: number;
+
+  @Field()
+  @Column({ name: 'CONTRACT' })
+  contract!: string;
+
+  @Field()
+  @Column({ name: 'DIVISION' })
+  division!: string;
+
+  @Field({ nullable: true })
+  @Column({ name: 'SALESMAN_CODE' })
+  salesmanCode?: string;
+
+  @Field({ nullable: true })
+  @Column({ name: 'PO_NUMBER' })
+  poNumber?: string;
+
+  @Field({ nullable: true })
+  @Column({ name: 'CURRENCY' })
+  currency?: string;
+
+  @Field({ nullable: true })
+  @Column({ name: 'DELIVERY_NOMINAL' })
+  deliveryNominal?: number;
+
+  @Field()
+  @Column({ name: 'CIRCULATION_DATE' })
+  circulationDate!: Date;
+
+  @Field()
+  @Column({ name: 'DESCRIPTION' })
+  description!: string;
+
+  @Field({ nullable: true })
+  @Column({ name: 'PAYMENT_METHOD' })
+  paymentMethod?: string;
+
+  @Field({ nullable: true })
+  @Column({ name: 'AR_PERSONNEL' })
+  arPersonnel?: string;
+
+  @Field()
+  @Column({ name: 'COURIER_SERVICE_NAME' })
+  courierServiceName!: string;
+
+  @Field()
+  @Column({ name: 'AWB_NO' })
+  awbNo!: string;
+
+  @Field()
+  @Column({ name: 'TYPE' })
+  type!: string;
+
+  @Field()
+  @Column({ name: 'SENDER' })
+  sender!: string;
+
+  @Field()
+  @Column({ name: 'DELIVERY_DATE' })
+  deliveryDate!: Date;
+
+  @Field()
+  @Column({ name: 'RECEIVER' })
+  receiver!: string;
+
+  @Field({ nullable: true })
+  @Column({ name: 'INVOICE_NO_HD' })
+  invoiceNoHd?: string;
+
+  @Field({ nullable: true })
+  @Column({ name: 'COURIER_FEE' })
+  courierFee?: number;
+
+  @Field({ nullable: true })
+  @Column({ name: 'INVOICE_NO_AR' })
+  invoiceNoAr?: string;
+
+  @Field({ nullable: true })
+  @Column({ name: 'NOTES' })
+  notes?: string;
+}
