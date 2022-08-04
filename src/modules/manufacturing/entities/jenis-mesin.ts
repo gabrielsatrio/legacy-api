9c271afa2ff66bbf93ed1ae8b482db5eeb6857ed
Christian Wibisono <christianwibisono@ateja.co.id>
2022-08-04 14:55:22 +0700

    feat: add jenis mesin entity and input file

---
+++ src/modules/manufacturing/entities/jenis-mesin.ts
@@ -0,0 +1,22 @@
+import { Field, ObjectType } from 'type-graphql';
+import { BaseEntity, Column, Entity, PrimaryColumn } from 'typeorm';
+
+@Entity('CHR_JENIS_MESIN_AT2')
+@ObjectType()
+export class JenisMesinAt2 extends BaseEntity {
+  @Field()
+  @PrimaryColumn({ name: 'CONTRACT' })
+  contract!: string;
+
+  @Field()
+  @PrimaryColumn({ name: 'MESIN' })
+  mesin!: string;
+
+  @Field({ nullable: true })
+  @Column({ name: 'JENIS' })
+  jenis?: string;
+
+  @Field()
+  @Column({ name: 'OBJ_ID' })
+  objId!: string;
+}
