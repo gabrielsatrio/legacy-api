fec6a6aba9d51d24918bb8cdf7117beb9b41fd97
angellia <angellia@ateja.co.id>
2022-08-04 16:18:26 +0700

    feat: absensi khl

---
+++ src/modules/general-affair/entities/absensi-khl-detail.vw.ts
@@ -0,0 +1,42 @@
+import { Field, Int, ObjectType } from 'type-graphql';
+import { BaseEntity, Column, Entity, PrimaryColumn } from 'typeorm';
+
+@Entity('ANG_EMPLOYEE_KHL_LINE_V')
+@ObjectType()
+export class AbsensiKhlDetailView extends BaseEntity {
+  @Field(() => Int)
+  @PrimaryColumn({ name: 'ID' })
+  id!: number;
+
+  @Field()
+  @Column({ name: 'NRP' })
+  nrp!: string;
+
+  @Field()
+  @Column({ name: 'NAME' })
+  name!: string;
+
+  @Field()
+  @Column({ name: 'GRADE' })
+  grade!: string;
+
+  @Field()
+  @Column({ name: 'ORG_ID' })
+  orgId!: string;
+
+  @Field()
+  @Column({ name: 'ORG_NAME' })
+  orgName!: string;
+
+  @Field()
+  @Column({ name: 'POS_ID' })
+  posId!: string;
+
+  @Field()
+  @Column({ name: 'POS_NAME' })
+  posName!: string;
+
+  @Field(() => Int)
+  @PrimaryColumn({ name: 'EMPLOYEE_KHL_ID' })
+  employeeKhlId!: number;
+}
