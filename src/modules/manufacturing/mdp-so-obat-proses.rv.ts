d5eda4aed996de7946fcc8134b15b5e26c948524
Christian Wibisono <christianwibisono@ateja.co.id>
2022-08-11 15:09:28 +0700

    feat: sent mail when so create agt

--- src/modules/manufacturing/mdp-so-obat-proses.rv.ts
+++ src/modules/manufacturing/mdp-so-obat-proses.rv.ts
@@ -1,3 +1,4 @@
+import config from '@/config/main';
 import { ifs } from '@/database/data-sources';
 import { isAuth } from '@/middlewares/is-auth';
 import { mapError } from '@/utils/map-error';
@@ -5,6 +6,7 @@ import dayjs from 'dayjs';
 import oracledb from 'oracledb';
 import { Arg, Mutation, Query, Resolver, UseMiddleware } from 'type-graphql';
 import { In } from 'typeorm';
+import { sendEmail } from '../../utils/send-email';
 import { SoObatProses } from './entities/mdp-so-obat-proses';
 import { SoObatProsesInput } from './mdp-so-obat-proses.in';
 import { SoObatProsesReceiveInput } from './mdp-so-obat-receive.in';
@@ -119,6 +121,17 @@ export class SoObatProsesResolver {
       const data = await SoObatProses.findOneBy({
         orderNo: outOrderNo
       });
+
+      await sendEmail(
+        `Deni Ramdani <deniramdani@agtex.co.id>` || '',
+        [],
+        [],
+        `Approval Request for Order No ${outOrderNo}`,
+        `<p>Dear Mr Deni Ramdani,</p>
+        <p>A new Order No (No: ${outOrderNo}) has been submitted for your approval.</br>
+        You can find all the details about this request by clicking <a href="${config.client.url}/m/017/apr"><b>here</b></a>.</br>
+        Please confirm your approval.</p>`
+      );
 
       return data;
     } catch (err) {
