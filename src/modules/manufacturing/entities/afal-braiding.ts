1d9428930ac1e3dd0cfb684b80fc38d6ae2a8e70
Christian Wibisono <christianwibisono@ateja.co.id>
2022-10-04 13:40:24 +0700

    feat: crud afal braiding

---
+++ src/modules/manufacturing/entities/afal-braiding.ts
@@ -0,0 +1,30 @@
+import { Field, ObjectType } from 'type-graphql';
+import { BaseEntity, Column, Entity, PrimaryColumn } from 'typeorm';
+
+@Entity('CHR_AFAL_BRAIDING')
+@ObjectType()
+export class AfalBraiding extends BaseEntity {
+  @Field()
+  @PrimaryColumn({ name: 'TANGGAL' })
+  tanggal!: Date;
+
+  @Field()
+  @PrimaryColumn({ name: 'CONTRACT' })
+  contract!: string;
+
+  @Field()
+  @Column({ name: 'PP' })
+  pp!: number;
+
+  @Field()
+  @Column({ name: 'POLYESTER' })
+  polyester!: number;
+
+  @Field()
+  @Column({ name: 'PVC' })
+  pvc!: number;
+
+  @Field()
+  @Column({ name: 'OBJ_ID' })
+  objId!: string;
+}
