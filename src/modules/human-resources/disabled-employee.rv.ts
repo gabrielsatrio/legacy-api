09b2fb19df291d83e04fb7e77815e734b3566c3e
angellia <angellia@ateja.co.id>
2023-01-09 16:52:32 +0700

    feat: add procedure insert & delete

--- src/modules/human-resources/disabled-employee.rv.ts
+++ src/modules/human-resources/disabled-employee.rv.ts
@@ -1,3 +1,4 @@
+import { ifs } from '@/database/data-sources';
 import { isAuth } from '@/middlewares/is-auth';
 import { mapError } from '@/utils/map-error';
 import { Arg, Mutation, Query, Resolver, UseMiddleware } from 'type-graphql';
@@ -7,18 +8,6 @@ import { DisabledEmployeesView } from './entities/disabled-employee.vw';
 
 @Resolver(DisabledEmployees)
 export class DisabledEmployeesResolver {
-  @Query(() => Boolean)
-  @UseMiddleware(isAuth)
-  async checkDisabledEmployeesExist(
-    @Arg('employeeId') employeeId: string
-  ): Promise<boolean> {
-    try {
-      return (await this.getDisabledEmployees(employeeId)) ? true : false;
-    } catch (err) {
-      throw new Error(mapError(err));
-    }
-  }
-
   @Query(() => [DisabledEmployeesView])
   @UseMiddleware(isAuth)
   async getAllDisabledEmployees(): Promise<
@@ -31,13 +20,15 @@ export class DisabledEmployeesResolver {
     }
   }
 
-  @Query(() => DisabledEmployeesView, { nullable: true })
+  @Mutation(() => Boolean)
   @UseMiddleware(isAuth)
-  async getDisabledEmployees(
-    @Arg('employeeId') employeeId: string
-  ): Promise<DisabledEmployeesView | null> {
+  async createDisabledEmployeesPostgre(
+    @Arg('input') input: DisabledEmployeesInput
+  ): Promise<boolean | null> {
     try {
-      return await DisabledEmployeesView.findOneBy({ employeeId });
+      const sql = `begin ang_disabled_employees_api.create__(:employeeId); end;`;
+      await ifs.query(sql, [input.employeeId]);
+      return true;
     } catch (err) {
       throw new Error(mapError(err));
     }
@@ -45,51 +36,14 @@ export class DisabledEmployeesResolver {
 
   @Mutation(() => DisabledEmployees)
   @UseMiddleware(isAuth)
-  async createDisabledEmployees(
-    @Arg('input') input: DisabledEmployeesInput
-  ): Promise<DisabledEmployees | undefined> {
-    try {
-      const existingData = await DisabledEmployees.findOneBy({
-        employeeId: input.employeeId
-      });
-      if (existingData) throw new Error('Data already exists.');
-      const data = DisabledEmployees.create({
-        ...input
-      });
-      const result = await DisabledEmployees.save(data);
-      return result;
-    } catch (err) {
-      throw new Error(mapError(err));
-    }
-  }
-
-  @Mutation(() => DisabledEmployees, { nullable: true })
-  @UseMiddleware(isAuth)
-  async updateDisabledEmployees(
-    @Arg('input') input: DisabledEmployeesInput
-  ): Promise<DisabledEmployees | undefined> {
-    try {
-      const data = await DisabledEmployees.findOneBy({
-        employeeId: input.employeeId
-      });
-      if (!data) throw new Error('No data found.');
-      DisabledEmployees.merge(data, { ...input });
-      const result = await DisabledEmployees.save(data);
-      return result;
-    } catch (err) {
-      throw new Error(mapError(err));
-    }
-  }
-
-  @Mutation(() => DisabledEmployees)
-  @UseMiddleware(isAuth)
-  async deleteDisabledEmployees(
+  async deleteDisabledEmployeesPostgre(
     @Arg('employeeId') employeeId: string
-  ): Promise<DisabledEmployees> {
+  ): Promise<DisabledEmployees | null> {
     try {
+      const sql = `begin ang_disabled_employees_api.delete__(:employeeId); end;`;
       const data = await DisabledEmployees.findOneBy({ employeeId });
       if (!data) throw new Error('No data found.');
-      await DisabledEmployees.delete({ employeeId });
+      await ifs.query(sql, [employeeId]);
       return data;
     } catch (err) {
       throw new Error(mapError(err));
