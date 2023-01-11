7c124ae8fe1f18f2d68c975882d4c04f64589025
vickyricardo <vickyricardo8@gmail.com>
2023-01-11 03:04:16 +0000

    feat: add get warehouse by site query

--- src/modules/warehouse/atj-ims-warehouse.rv.ts
+++ src/modules/warehouse/atj-ims-warehouse.rv.ts
@@ -1,6 +1,6 @@
 import { isAuth } from '@/middlewares/is-auth';
 import { mapError } from '@/utils/map-error';
-import { Query, Resolver, UseMiddleware } from 'type-graphql';
+import { Arg, Query, Resolver, UseMiddleware } from 'type-graphql';
 import { ImsWarehouseBayBinView } from './entities/atj-ims-warehoouse.vw';
 import { ImsWarehouseBayBin } from './entities/atj-ims-warehouse';
 
@@ -11,6 +11,18 @@ export class ImsWarehouseBayBinResolver {
   async getAllImsWarehouse(): Promise<ImsWarehouseBayBinView[] | undefined> {
     try {
       return await ImsWarehouseBayBinView.find();
+    } catch (err) {
+      throw new Error(mapError(err));
+    }
+  }
+
+  @Query(() => [ImsWarehouseBayBinView])
+  @UseMiddleware(isAuth)
+  async getImsWarehouseBySite(
+    @Arg('contract', () => String) contract: string
+  ): Promise<ImsWarehouseBayBinView[] | undefined> {
+    try {
+      return await ImsWarehouseBayBinView.findBy({ contract });
     } catch (err) {
       throw new Error(mapError(err));
     }
