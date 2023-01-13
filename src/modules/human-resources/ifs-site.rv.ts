4b15bc71dbb0725824b3908d38e9fa863dc26705
vickyricardo <vickyricardo8@gmail.com>
2023-01-13 08:11:46 +0000

    Revert "fix: query"
    This reverts commit 528346300f65ae91ff14295b6dad884c10cd87dd.

--- src/modules/human-resources/ifs-site.rv.ts
+++ src/modules/human-resources/ifs-site.rv.ts
@@ -1,4 +1,3 @@
-import { ifs } from '@/database/data-sources';
 import { isAuth } from '@/middlewares/is-auth';
 import { mapError } from '@/utils/map-error';
 import { Query, Resolver, UseMiddleware } from 'type-graphql';
@@ -10,22 +9,7 @@ export class IfsSiteResolver {
   @UseMiddleware(isAuth)
   async getAllSite(): Promise<SiteTab[] | undefined> {
     try {
-      return await ifs.query(`
-        SELECT    contract AS "contract",
-                  delivery_address AS "deliveryAddress",
-                  company AS "company",
-                  dist_calendar_id AS "distCalendarId",
-                  manuf_calendar_id AS "manufCalendarId",
-                  offset AS "offset",
-                  rowversion AS "rowversion",
-                  rowkey AS "rowkey",
-                  ctm_contract_alias AS "ctmContractAlias"
-        FROM     (SELECT *
-                  FROM   site_tab
-                  UNION
-                  SELECT *
-                  FROM   site_tab@ifs8agt)
-        ORDER BY contract ASC`);
+      return await SiteTab.find({ order: { contract: 'ASC' } });
     } catch (err) {
       throw new Error(mapError(err));
     }
