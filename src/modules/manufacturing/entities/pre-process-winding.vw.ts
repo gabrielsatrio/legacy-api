a31adf0e822f05e5b00a2c0a6202d866fa87c13d
vickyricardo <vickyricardo8@gmail.com>
2023-02-07 04:23:55 +0000

    feat: pre process winding entity and resolver

---
+++ src/modules/manufacturing/entities/pre-process-winding.vw.ts
@@ -0,0 +1,38 @@
+import { Field, ObjectType } from 'type-graphql';
+import { BaseEntity, Column, Entity, PrimaryColumn } from 'typeorm';
+
+@Entity('VKY_PRE_PROCESS_WINDING_V')
+@ObjectType()
+export class PreProcessWindingView extends BaseEntity {
+  @Field()
+  @PrimaryColumn({ name: 'CONTRACT' })
+  contract!: string;
+
+  @Field()
+  @PrimaryColumn({ name: 'PART_NO' })
+  partNo!: string;
+
+  @Field()
+  @PrimaryColumn({ name: 'LOT_BATCH_NO' })
+  lotBatchNo!: string;
+
+  @Field({ nullable: true })
+  @Column({ name: 'PIECE_NO' })
+  pieceNo?: string;
+
+  @Field()
+  @Column({ name: 'GROSS' })
+  gross!: number;
+
+  @Field()
+  @Column({ name: 'NETT' })
+  nett!: number;
+
+  @Field({ nullable: true })
+  @Column({ name: 'JOB_ORDER' })
+  jobOrder?: string;
+
+  @Field()
+  @Column({ name: 'SCAN_DATE' })
+  scanDate!: Date;
+}
