2cf5e57363633a3d0c9d39797f6746aa0aa9c2a7
angellia <angellia@ateja.co.id>
2023-02-15 11:43:50 +0700

    feat: faktur pajak

---
+++ src/modules/financials/entities/faktur-pajak.vw.ts
@@ -0,0 +1,38 @@
+import { Field, Int, ObjectType } from 'type-graphql';
+import { BaseEntity, Column, Entity, PrimaryColumn } from 'typeorm';
+
+@Entity('ANG_FAKTUR_PAJAK_V')
+@ObjectType()
+export class FakturPajakView extends BaseEntity {
+  @Field(() => Int)
+  @PrimaryColumn({ name: 'ID' })
+  id!: number;
+
+  @Field({ nullable: true })
+  @Column({ name: 'KODE_FAKTUR' })
+  kodeFaktur?: string;
+
+  @Field({ nullable: true })
+  @Column({ name: 'CONTRACT' })
+  contract?: string;
+
+  @Field({ nullable: true })
+  @Column({ name: 'ASSOCIATION_NO' })
+  associationNo?: string;
+
+  @Field({ nullable: true })
+  @Column({ name: 'CUSTOMER_NO' })
+  customerNo?: string;
+
+  @Field({ nullable: true })
+  @Column({ name: 'CUSTOMER_NAME' })
+  customerName?: string;
+
+  @Field({ nullable: true })
+  @Column({ name: 'DATE_DELIVERED' })
+  dateDelivered?: Date;
+
+  @Field({ nullable: true })
+  @Column({ name: 'NO_SJ' })
+  noSj?: string;
+}
---
+++ src/modules/financials/faktur-pajak.in.ts
@@ -0,0 +1,38 @@
+import { IsDate, IsNumber } from 'class-validator';
+import { Field, InputType, Int } from 'type-graphql';
+import { FakturPajak } from './entities/faktur-pajak';
+
+@InputType()
+export class FakturPajakInput implements Partial<FakturPajak> {
+  @Field(() => Int)
+  @IsNumber()
+  id!: number;
+
+  @Field({ nullable: true })
+  kodeFakturA?: string;
+
+  @Field({ nullable: true })
+  kodeFakturB?: string;
+
+  @Field({ nullable: true })
+  kodeFakturC?: string;
+
+  @Field({ nullable: true })
+  contract?: string;
+
+  @Field({ nullable: true })
+  associationNo?: string;
+
+  @Field({ nullable: true })
+  customerNo?: string;
+
+  @Field({ nullable: true })
+  customerName?: string;
+
+  @Field({ nullable: true })
+  @IsDate()
+  dateDelivered?: Date;
+
+  @Field({ nullable: true })
+  noSj?: string;
+}
