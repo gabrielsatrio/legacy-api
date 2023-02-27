426b92492d165f0aa55ebf939f4bd2ee8a093ff4
angellia <angellia@ateja.co.id>
2023-02-27 16:57:03 +0700

    fix: merge

--- src/modules/financials/kasbon-bank.in.ts
+++
@@ -1,8 +0,0 @@
-import { Field, InputType } from 'type-graphql';
-import { BankKasbon } from './entities/kasbon-bank';
-
-@InputType()
-export class BankKasbonInput implements Partial<BankKasbon> {
-  @Field()
-  bank!: string;
-}
--- src/modules/financials/kasbon-dept.in.ts
+++
@@ -1,8 +0,0 @@
-import { Field, InputType } from 'type-graphql';
-import { DeptKasbon } from './entities/kasbon-dept';
-
-@InputType()
-export class DeptKasbonInput implements Partial<DeptKasbon> {
-  @Field()
-  department!: string;
-}
