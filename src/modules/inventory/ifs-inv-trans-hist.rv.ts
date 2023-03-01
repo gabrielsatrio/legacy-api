b17e6cce36a0254ecb83ebd95e80231f3e00dc5f
vickyricardo <vickyricardo8@gmail.com>
2023-03-01 08:06:23 +0000

    refactor: move generate kain premium tmp to its own resolver

--- src/modules/inventory/ifs-inv-trans-hist.rv.ts
+++ src/modules/inventory/ifs-inv-trans-hist.rv.ts
@@ -1,7 +1,5 @@
-import { ifs } from '@/database/data-sources';
 import { isAuth } from '@/middlewares/is-auth';
-import { mapError } from '@/utils/map-error';
-import { Arg, Mutation, Query, Resolver, UseMiddleware } from 'type-graphql';
+import { Arg, Query, Resolver, UseMiddleware } from 'type-graphql';
 import { IfsInventoryTransactionHistoryView } from '../inventory/entities/ifs-inv-trans-hist.vw';
 
 @Resolver(IfsInventoryTransactionHistoryView)
@@ -41,23 +39,5 @@ export class IfsTransactionHistoryResolver {
       })
       .andWhere(`TH.QTY_REVERSED= 0`)
       .getMany();
-  }
-
-  @Mutation(() => Boolean)
-  @UseMiddleware(isAuth)
-  async generateKainPremiumTmp(
-    @Arg('date', () => Date) date: Date
-  ): Promise<boolean | null> {
-    try {
-      const sql = `
-        BEGIN
-          vky_kain_premium_tmp_api.insert__( :p_from);
-        END;
-      `;
-      await ifs.query(sql, [date]);
-      return true;
-    } catch (err) {
-      throw new Error(mapError(err));
-    }
   }
 }
---
+++ src/modules/manufacturing/entities/kain-premium-tmp.ts
@@ -0,0 +1,50 @@
+import { Field, ObjectType } from 'type-graphql';
+import { BaseEntity, Column, Entity, PrimaryColumn } from 'typeorm';
+
+@Entity('VKY_KAIN_PREMIUM_TMP')
+@ObjectType()
+export class KainPremiumTmp extends BaseEntity {
+  @Field()
+  @PrimaryColumn({ name: 'PART_NO' })
+  partNo!: string;
+
+  @Field()
+  @PrimaryColumn({ name: 'LOT_BATCH_NO' })
+  lotBatchNo!: string;
+
+  @Field()
+  @Column({ name: 'GROSS_LENGTH' })
+  grossLength!: number;
+
+  @Field()
+  @Column({ name: 'QUANTITY' })
+  quantity!: number;
+
+  @Field({ nullable: true })
+  @Column({ name: 'PIECE_NO' })
+  pieceNo?: string;
+
+  @Field({ nullable: true })
+  @Column({ name: 'JOB_ORDER' })
+  jobOrder?: string;
+
+  @Field({ nullable: true })
+  @Column({ name: 'CUT_QTY' })
+  cutQty?: number;
+
+  @Field({ nullable: true })
+  @Column({ name: 'DEFECT_QTY' })
+  defectQty?: number;
+
+  @Field({ nullable: true })
+  @Column({ name: 'LOT_BATCH_SOURCE' })
+  lotBatchSource?: string;
+
+  @Field({ nullable: true })
+  @Column({ name: 'LOCATION_NO' })
+  locationNo?: string;
+
+  @Field({ nullable: true })
+  @Column({ name: 'JENIS_KAIN' })
+  jenisKain?: string;
+}
